a340cf9aef4a3939f4b7759d26beb4f5eb3c8f9f
Patipan Mata <patipan.ma@arise.tech>
2023-12-30 10:09:53 +0700

    Fixed : conflict test1.jsx

--- test1.jsx
+++ test1.jsx
@@ -6,6 +6,17 @@ function test1() {
         <div>test1</div>
         <h2>karn</h2>
         <p>Want a coffee!</p>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
+        <h3>Edited</h3>
         </div>
     )
 }
